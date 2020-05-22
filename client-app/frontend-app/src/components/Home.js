6e04f4899b4170403bcea90de7507e1b0875895c
san <hillsfrontendweb@gmail.com>
2020-05-21 21:53:09 -0400

    building grid system

--- client-app/frontend-app/src/components/Home.js
+++ client-app/frontend-app/src/components/Home.js
@@ -153,170 +153,36 @@ function Home() {
   // const computeTimeDurationSection = <p>Hello world</p>
 
   return (
-    <div className="Home">
+    
+    //nav bar
+    <div className="Home app-background">
+
       <nav class="sticky-top navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
           Navbar
         </a>
       </nav>
+
+      <div class="container-fluid">
+        <div class="row mb-1">
+          <div class="col m-3">col1</div>
+          <div class="col m-3">col2</div>
+          <div class="col m-3">col3</div>
+          <div class="col m-3">col4</div>
+        </div>
+
+        <div class="row mb-5">
+          <div class="col m-3">col1</div>
+          <div class="col m-3">col2</div>
+          <div class="col m-3">col3</div>
+          <div class="col m-3">col4</div>
+        </div>
+      </div>
+
+    </div>  
       
         
-        <div class="row">
-        <div class="col-2 light-gray"></div>
-          <div class="col-sm-4">
-              
-              <div>
-                Name:
-                <br />
-                {username}
-                <br />
-                <br />
-                  <h3>Short Bio</h3>
-                  {username} has a degree from a university. He is an expert in almost
-                  nothing.
-                <br />
-                <br />
-                <h3>Long Bio</h3>
-                If {username} was more accomplished. This would be a much longer
-                section.
-
-                <br />
-                <br />
-                <p>Please select how you would like to talk to {username} by clicking the
-                Select Medium button. You can also dictate how long you would like to
-                talk to {username} by clicking the Select Duration button. By default,
-                a 10 minute messaging conversation is selected, but users often prefer
-                phone, video, and longer periods of time.
-                </p>
-              </div>
-          </div>
-        
-        <div class="col-sm">
-            <img class="img-fluid"
-              className="pictureSection"
-              src={process.env.PUBLIC_URL + "/assets/profile.jpg"}
-              alt="picture of user"
-            />
-        </div>
-        </div>
-      
-      
-
-      <div className="bigParagraph">
-        
-      </div>
-      <br />
-
-      <div className="baseball">
-        <div className="bigParagraph">
-          
-        </div>
-        <br />
-        <br />
-        <table className="tableStyles">
-          <tbody>
-            <tr className="tableRow">
-              <th>Medium (for communication)</th>
-              <th>Time (in minutes)</th>
-            </tr>
-            <tr>
-              <td className="tableRow">
-                <button
-                  type="button"
-                  onClick={(e) => handleMediumDropdownButtonClick(e)}
-                >
-                  Select Medium
-                </button>
-                {openMedium && (
-                  <div>
-                    <ul className="ourList">{mediumList}</ul>
-                  </div>
-                )}
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={(e) => handleDurationDropdownButtonClick(e)}
-                >
-                  Select Duration
-                </button>
-                {openDuration && (
-                  <div>
-                    <ul className="ourList">{durationsList}</ul>
-                  </div>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        {printedComputeStatement}
-        <div className="bigParagraph">
-          Intro message for {username}. Feel free to introduce yourself to{" "}
-          {username}, ask questions you are hoping to be answered, or write
-          whatever you would like! (optional) (1000 word limit):
-        </div>
-        <div>
-          <br />
-          <br />
-          <textarea
-            className="bigTextArea"
-            type="textarea"
-            onChange={(e) => setIntro(e.target.value)}
-          />
-        </div>
-      </div>
-      <br />
-      <div>
-        {username}s Available Times
-        <br />
-        <br />
-        <table className="tableStyles">
-          <tbody>
-            <tr className="tableRow">
-              <th>Date</th>
-              <th>Time (EST)</th>
-            </tr>
-            <tr>
-              <td className="tableRow">
-                <button
-                  type="button"
-                  onClick={(e) => handleDateDropdownButtonClick(e)}
-                >
-                  Select From Available Dates
-                </button>
-                {openDate && (
-                  <div>
-                    <ul className="ourList">{datesList}</ul>
-                  </div>
-                )}
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={(e) => handleTimeDropdownButtonClick(e)}
-                >
-                  Select From Available Times
-                </button>
-                {openTime && (
-                  <div>
-                    <ul className="ourList">{timesList}</ul>
-                  </div>
-                )}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        {computeTimeDurationSection}
-      </div>
-
-      <br />
-      <div>
-        <br />
-        <button onClick={(e) => handleFinalSubmit(e)}>
-          Purchase Conversation
-        </button>
-      </div>
-    </div>
+ 
   );
 }
 
